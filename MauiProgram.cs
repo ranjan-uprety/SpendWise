c4c2b20cc62bd944cfc61b2d865c9a0cf7e5e588
ranjan-uprety <ranjanuprety01@gmail.com>
2025-08-08 18:02:23 +0545

    Add remaining DTOs, resources, MAUI files

---
+++ MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using SpendWise.Services;
+using Radzen;
+
+namespace SpendWise
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddSingleton<IUserService, UserService>();
+            builder.Services.AddSingleton<ITransactionService, TransactionService>();
+            builder.Services.AddSingleton<IDebtService, DebtService>();
+            builder.Services.AddSingleton<ITransactionHelperService, TransactionHelperService>();
+            builder.Services.AddSingleton<AuthenticationStateService>();
+            builder.Services.AddRadzenComponents();
+
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
